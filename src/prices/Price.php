2216d1557fe99768ae8e23ad08802d689a15bdd8
Ondřej Šibrava <ondrej.sibrava@varhall.cz>
2018-10-19 16:51:37 +0200

    sum prices fix

--- src/prices/Price.php
+++ src/prices/Price.php
@@ -77,102 +77,11 @@ class Price extends AbstractPrice
         if ($this->vat !== $price->vat)
             return PriceGroup::create([ $this, $price ]);
 
-        return static::base($this->price + $price->price(), $this->vat, $this->discount + $price->discount());
+        return static::base($this->price + $price->price, $this->vat, $this->discount + $price->discount);
     }
 
     public function multiply($count)
     {
         return static::base($this->price * $count, $this->vat, $this->discount * $count);
     }
-
-
-
-    /*
-    public abstract function discount();
-
-    public abstract function vat();
-
-    public abstract function price();
-
-    public abstract function price_vat();
-
-    public abstract function price_discount();
-
-    public abstract function price_discount_vat();
-
-
-    protected function applyVat($price)
-    {
-        return $price * (1 + ($this->vat / 100));
-    }*/
-
-
-    /*public $price       = 0;
-
-    public $vat         = 0;
-
-    public $discount    = 0;
-
-
-    public function __construct($price = 0, $vat = 0, $discount = 0)
-    {
-        $this->price = $price;
-        $this->vat = $vat;
-        $this->discount = $discount;
-    }
-
-    public function __get($name)
-    {
-        if (method_exists($this, $name))
-            return call_user_func([$this, $name]);
-
-        throw new \BadMethodCallException("Property $name not found");
-    }
-
-    /// PRICES
-
-    public function price_vat()
-    {
-        return $this->applyVat($this->price);
-    }
-
-    public function price_discount()
-    {
-        return max($this->price - $this->discount, 0);
-    }
-
-    public function price_discount_vat()
-    {
-        return $this->applyVat($this->price_discount());
-    }
-
-    protected function applyVat($price)
-    {
-        return $price * (1 + ($this->vat / 100));
-    }
-
-
-    /// OPERATIONS
-
-    public function add(Price $price)
-    {
-        if ($this->vat !== $price->vat)
-            throw new \InvalidArgumentException('VATs must be same');
-
-        $this->price += $price->price;
-        $this->sale += $price->sale;
-
-        return $this;
-    }
-
-    public function multiply($count)
-    {
-        $this->price *= $count;
-        $this->sale *= $count;
-
-        return $this;
-    }*/
-
-
-
 }
