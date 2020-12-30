357a4f0f62c4c84f6f06a2092bb62988b95a8260
Ondrej Sibrava <ondrej.sibrava@varhall.cz>
2020-12-30 10:09:31 +0100

    recursive property read

--- src/utils/Reflection.php
+++ src/utils/Reflection.php
@@ -7,19 +7,30 @@ trait Reflection
 {
     protected function readPrivateProperty($object, $property)
     {
-        $class = new \ReflectionClass(get_class($object));
-        $prop = $class->getProperty($property);
-        $prop->setAccessible(true);
-
-        return $prop->getValue($object);
+        return $this->getPrivateProperty($object, $property)->getValue($object);
     }
 
     protected function writePrivateProperty($object, $property, $value)
     {
-        $class = new \ReflectionClass(get_class($object));
-        $prop = $class->getProperty($property);
-        $prop->setAccessible(true);
+        $this->getPrivateProperty($object, $property)->setValue($object, $value);
+    }
 
-        $prop->setValue($object, $value);
+    protected function getPrivateProperty($object, $property)
+    {
+        $class = new \ReflectionClass(get_class($object));
+
+        while ($class) {
+            if (!$class->hasProperty($property)) {
+                $class = $class->getParentClass();
+                continue;
+            }
+
+            $prop = $class->getProperty($property);
+            $prop->setAccessible(true);
+
+            return $prop;
+        }
+
+        throw new \ReflectionException("Property {$property} does not exist");
     }
 }
